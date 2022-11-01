2e0021900fc9d5077008a0ccc8397b474cb2c314
Ira114822 <odaa1976.com@gmail.com>
2022-11-01 20:16:26 +0300

    Initial commit

---
+++ manager.py
@@ -0,0 +1,19 @@
+from Logic import Logic
+from rectangel_creator import RectangleCreator
+from view import View
+
+
+def main():
+    size = 5
+    ls = RectangleCreator.get_rectangles(size)
+    total_square = Logic.calculate_total_square(ls)
+    total_perimetr = Logic.calculate_total_perimetr(ls)
+
+    msg = (f"Total square = {total_square}\n" \
+           f"total perimetr = {total_perimetr}")
+    print(View.convert(ls))
+    print(msg)
+
+
+if __name__ == "__main__":
+    main()
---
+++ rectangel_creator.py
@@ -0,0 +1,19 @@
+# утилитный класс который возвращает объект
+import random
+from rectangel import Rectangle
+
+class RectangleCreator:
+    FIRST_SIDE_RANGE = 10
+    SECOND_SIDE_RANGE = 20
+
+
+    def get_rectangles(size):
+        rectangles = []
+
+        for _ in range(size):
+            rect = Rectangle()
+            rect.a = random.randint(1, RectangleCreator.FIRST_SIDE_RANGE)
+            rect.b = random.randint(1, RectangleCreator.SECOND_SIDE_RANGE)
+            rectangles.append(rect)
+
+        return rectangles
